62d80eb56b7a520d139089ab4d0ae3dec5eb0cba
Turpster <dicky_056@hotmail.co.uk>
2018-12-07 14:35:39 +0000

    Made a start with files

--- College Programming/Files/Papers/RecordStructuresExercise/Tasks/TaskOne.cs
+++ College Programming/Files/Papers/RecordStructuresExercise/Tasks/TaskOne.cs
@@ -8,9 +8,27 @@ namespace Files.Papers.RecordStructuresExercise.Tasks
                                 "Create a record structure to hold this data."
             , "Task One") {}
 
+        public struct Book
+        {
+            public int ISBNNumber;
+            public string Title;
+            public string Author;
+            public int YearOfPublic;
+
+            public Book(int isbnNumber, string title, string author, int yearOfPublic)
+            {
+                ISBNNumber = isbnNumber;
+                Title = title;
+                Author = author;
+                YearOfPublic = yearOfPublic;
+            }
+        }
+
         public override void Run()
         {
-            throw new System.NotImplementedException();
+            Book book;
+            int num = Utils.AskUserInteger("ISBNNumber");
+            book = new Book();
         }
     }
 }
